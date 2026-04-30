d9da1f57f382efa002deb62c0e3498a82bdd9030
roederdt <Roederdt@gmail.com>
2026-04-29 20:21:40 -0400

    test again

--- src/app/reviews/ReviewCard.tsx
+++ src/app/reviews/ReviewCard.tsx
@@ -244,7 +244,7 @@ const ReviewForm: React.FC<{}> = () => {
                   key={star}
                   type="button"
                   onClick={() => setRating(star)}
-                  className="mt-2 text-yellow-500 text-4xl focus:outline-none"
+                  className="mt-2 text-yellow-500 text-3xl focus:outline-none"
                 >
                   {star <= rating ? '★' : '☆'}
                 </button>
